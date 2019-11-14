52e8743e220a6a6b2dce754fd40e86f0b1e33c0f
burato42 <tmahmutov@gmail.com>
2019-11-15 01:02:27 +0700

    Revert fabric to a ported version

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,57 +1,51 @@
 import random
-from fabric.connection import Connection
-from patchwork.files import exists, append
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
 
 
-def deploy(con):
-    site_folder = f'/home/{con.user}/sites/{con.host}'
-    con.run(f'mkdir -p {site_folder}')
-    with con.cd(site_folder):
-        _get_latest_source(con)
-        _update_virtualenv(con)
-        _create_or_update_dotenv(con)
-        _update_static_files(con)
-        _update_database(con)
+REPO_URL = 'https://github.com/burato42/superlists.git'
 
 
-def _get_latest_source(сon):
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+
+def _get_latest_source():
     if exists('.git'):
-        сon.run('git fetch')
+        run('git fetch')
     else:
-        сon.run(f'git clone {REPO_URL} .')
-    current_commit = сon.local("git log -n 1 --format=%H", capture=True)
-    сon.run(f'git reset --hard {current_commit}')
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
 
 
-def _update_virtualenv(сon):
+def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
-        сon.run(f'python3.6 -m venv virtualenv')
-    сon.run('./virtualenv/bin/pip install -r requirements.txt')
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt')
 
 
-def _create_or_update_dotenv(сon):
+def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={сon.host}')
-    current_contents = сon.run('cat .env')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        сon.append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 
-def _update_static_files(сon):
-    сon.run('./virtualenv/bin/python manage.py collectstatic --noinput')
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 
-def _update_database(сon):
-    сon.run('./virtualenv/bin/python manage.py migrate --noinput')
-
-
-if __name__ == '__main__':
-    host = 'superlists-staging.tmrm.me'
-    c = Connection(host=host, user='burato42')
-
-    REPO_URL = 'https://github.com/burato42/superlists.git'
-
-    deploy(c)
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
